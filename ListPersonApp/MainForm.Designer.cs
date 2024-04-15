b11e9d90b2d38b3a0c534d5c07f253631cd3ba8a
IIIsa <SuharevIA@mer.ci.nsu.ru>
2024-04-16 00:07:29 +0300

    Добавьте файлы проекта.

---
+++ ListPersonApp/MainForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ListPersonApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            AddButton = new Button();
+            RecordListLabel = new Label();
+            usersAmount = new TextBox();
+            PanelUsers = new FlowLayoutPanel();
+            ageSum = new TextBox();
+            SumAgeLabel = new Label();
+            SuspendLayout();
+            // 
+            // AddButton
+            // 
+            AddButton.Location = new Point(32, 18);
+            AddButton.Margin = new Padding(2);
+            AddButton.Name = "AddButton";
+            AddButton.Size = new Size(125, 41);
+            AddButton.TabIndex = 0;
+            AddButton.Text = "Добавить";
+            AddButton.UseVisualStyleBackColor = true;
+            AddButton.Click += Add_button;
+            // 
+            // RecordListLabel
+            // 
+            RecordListLabel.AutoSize = true;
+            RecordListLabel.Location = new Point(196, 31);
+            RecordListLabel.Margin = new Padding(2, 0, 2, 0);
+            RecordListLabel.Name = "RecordListLabel";
+            RecordListLabel.Size = new Size(119, 15);
+            RecordListLabel.TabIndex = 1;
+            RecordListLabel.Text = "Количество записей";
+            // 
+            // usersAmount
+            // 
+            usersAmount.Location = new Point(349, 31);
+            usersAmount.Margin = new Padding(2);
+            usersAmount.Name = "usersAmount";
+            usersAmount.ReadOnly = true;
+            usersAmount.Size = new Size(110, 23);
+            usersAmount.TabIndex = 2;
+            // 
+            // PanelUsers
+            // 
+            PanelUsers.AutoScroll = true;
+            PanelUsers.BackColor = Color.White;
+            PanelUsers.Location = new Point(22, 95);
+            PanelUsers.Margin = new Padding(2);
+            PanelUsers.MaximumSize = new Size(771, 355);
+            PanelUsers.Name = "PanelUsers";
+            PanelUsers.Size = new Size(771, 355);
+            PanelUsers.TabIndex = 3;
+            // 
+            // ageSum
+            // 
+            ageSum.Location = new Point(631, 31);
+            ageSum.Margin = new Padding(2);
+            ageSum.Name = "ageSum";
+            ageSum.ReadOnly = true;
+            ageSum.Size = new Size(110, 23);
+            ageSum.TabIndex = 5;
+            // 
+            // SumAgeLabel
+            // 
+            SumAgeLabel.AutoSize = true;
+            SumAgeLabel.Location = new Point(517, 34);
+            SumAgeLabel.Margin = new Padding(2, 0, 2, 0);
+            SumAgeLabel.Name = "SumAgeLabel";
+            SumAgeLabel.Size = new Size(93, 15);
+            SumAgeLabel.TabIndex = 4;
+            SumAgeLabel.Text = "Количество лет";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(816, 461);
+            Controls.Add(ageSum);
+            Controls.Add(SumAgeLabel);
+            Controls.Add(PanelUsers);
+            Controls.Add(usersAmount);
+            Controls.Add(RecordListLabel);
+            Controls.Add(AddButton);
+            Margin = new Padding(2);
+            Name = "MainForm";
+            Text = "Список пользователей";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button AddButton;
+        private Label RecordListLabel;
+        private TextBox usersAmount;
+        private FlowLayoutPanel PanelUsers;
+        private TextBox ageSum;
+        private Label SumAgeLabel;
+    }
+}
---
+++ PersonControlLibrary/PersonUserControl.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace PersonControlLib
+{
+    partial class PersonUserControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            idText = new TextBox();
+            IDLabel = new Label();
+            SurnameLabel = new Label();
+            surnameText = new TextBox();
+            NameLabel = new Label();
+            nameText = new TextBox();
+            LastNameLabel = new Label();
+            lastNameText = new TextBox();
+            AgeLabel = new Label();
+            ageText = new TextBox();
+            buttonEdit = new Button();
+            buttonDel = new Button();
+            SuspendLayout();
+            // 
+            // idText
+            // 
+            idText.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            idText.Location = new Point(28, 40);
+            idText.Margin = new Padding(3, 2, 3, 2);
+            idText.Name = "idText";
+            idText.ReadOnly = true;
+            idText.Size = new Size(47, 29);
+            idText.TabIndex = 0;
+            // 
+            // IDLabel
+            // 
+            IDLabel.AutoSize = true;
+            IDLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            IDLabel.Location = new Point(28, 10);
+            IDLabel.Name = "IDLabel";
+            IDLabel.Size = new Size(25, 21);
+            IDLabel.TabIndex = 1;
+            IDLabel.Text = "ID";
+            // 
+            // SurnameLabel
+            // 
+            SurnameLabel.AutoSize = true;
+            SurnameLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            SurnameLabel.Location = new Point(106, 10);
+            SurnameLabel.Name = "SurnameLabel";
+            SurnameLabel.Size = new Size(75, 21);
+            SurnameLabel.TabIndex = 3;
+            SurnameLabel.Text = "Фамилия";
+            // 
+            // surnameText
+            // 
+            surnameText.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            surnameText.Location = new Point(88, 40);
+            surnameText.Margin = new Padding(3, 2, 3, 2);
+            surnameText.Name = "surnameText";
+            surnameText.ReadOnly = true;
+            surnameText.Size = new Size(132, 29);
+            surnameText.TabIndex = 2;
+            // 
+            // NameLabel
+            // 
+            NameLabel.AutoSize = true;
+            NameLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            NameLabel.Location = new Point(271, 10);
+            NameLabel.Name = "NameLabel";
+            NameLabel.Size = new Size(41, 21);
+            NameLabel.TabIndex = 5;
+            NameLabel.Text = "Имя";
+            // 
+            // nameText
+            // 
+            nameText.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            nameText.Location = new Point(233, 40);
+            nameText.Margin = new Padding(3, 2, 3, 2);
+            nameText.Name = "nameText";
+            nameText.ReadOnly = true;
+            nameText.Size = new Size(132, 29);
+            nameText.TabIndex = 4;
+            // 
+            // LastNameLabel
+            // 
+            LastNameLabel.AutoSize = true;
+            LastNameLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            LastNameLabel.Location = new Point(401, 10);
+            LastNameLabel.Name = "LastNameLabel";
+            LastNameLabel.Size = new Size(77, 21);
+            LastNameLabel.TabIndex = 7;
+            LastNameLabel.Text = "Отчество";
+            // 
+            // lastNameText
+            // 
+            lastNameText.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lastNameText.Location = new Point(378, 40);
+            lastNameText.Margin = new Padding(3, 2, 3, 2);
+            lastNameText.Name = "lastNameText";
+            lastNameText.ReadOnly = true;
+            lastNameText.Size = new Size(132, 29);
+            lastNameText.TabIndex = 6;
+            // 
+            // AgeLabel
+            // 
+            AgeLabel.AutoSize = true;
+            AgeLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            AgeLabel.Location = new Point(531, 10);
+            AgeLabel.Name = "AgeLabel";
+            AgeLabel.Size = new Size(66, 21);
+            AgeLabel.TabIndex = 9;
+            AgeLabel.Text = "Возраст";
+            // 
+            // ageText
+            // 
+            ageText.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            ageText.Location = new Point(522, 40);
+            ageText.Margin = new Padding(3, 2, 3, 2);
+            ageText.Name = "ageText";
+            ageText.ReadOnly = true;
+            ageText.Size = new Size(82, 29);
+            ageText.TabIndex = 8;
+            // 
+            // buttonEdit
+            // 
+            buttonEdit.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonEdit.Location = new Point(623, 14);
+            buttonEdit.Margin = new Padding(3, 2, 3, 2);
+            buttonEdit.Name = "buttonEdit";
+            buttonEdit.Size = new Size(104, 28);
+            buttonEdit.TabIndex = 10;
+            buttonEdit.Text = "Изменить";
+            buttonEdit.UseVisualStyleBackColor = true;
+            // 
+            // buttonDel
+            // 
+            buttonDel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonDel.Location = new Point(623, 46);
+            buttonDel.Margin = new Padding(3, 2, 3, 2);
+            buttonDel.Name = "buttonDel";
+            buttonDel.Size = new Size(104, 28);
+            buttonDel.TabIndex = 11;
+            buttonDel.Text = "Удалить";
+            buttonDel.UseVisualStyleBackColor = true;
+            // 
+            // PersonUserControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(buttonDel);
+            Controls.Add(buttonEdit);
+            Controls.Add(AgeLabel);
+            Controls.Add(ageText);
+            Controls.Add(LastNameLabel);
+            Controls.Add(lastNameText);
+            Controls.Add(NameLabel);
+            Controls.Add(nameText);
+            Controls.Add(SurnameLabel);
+            Controls.Add(surnameText);
+            Controls.Add(IDLabel);
+            Controls.Add(idText);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "PersonUserControl";
+            Size = new Size(737, 83);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox idText;
+        private Label IDLabel;
+        private Label SurnameLabel;
+        private TextBox surnameText;
+        private Label NameLabel;
+        private TextBox nameText;
+        private Label LastNameLabel;
+        private TextBox lastNameText;
+        private Label AgeLabel;
+        private TextBox ageText;
+        private Button buttonEdit;
+        private Button buttonDel;
+    }
+}
